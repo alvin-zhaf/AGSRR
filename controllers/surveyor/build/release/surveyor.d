708f3206ebfd7f36a6fbf3f9c1275fe227883d47
alfian-f <alfian.0615@gmail.com>
2025-12-06 21:56:32 +0000

    cleaned build files

--- controllers/surveyor/build/release/surveyor.d
+++
@@ -1,8 +0,0 @@
-build/release/surveyor.o: surveyor.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/gps.h \
- C:\Program\ Files\Webots/include/controller/c/webots/compass.h \
- C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h
